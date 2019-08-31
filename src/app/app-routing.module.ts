c4085475a6e606fa6cbc5f1b6bc4833ebc30b061
mukesh-rana <mukeshrana90@gmail.com>
2019-08-31 20:31:33 +0530

    Components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from './backend/users/users.component';
+import { OrdersComponent } from './backend/orders/orders.component';
+import { ServicesComponent } from './backend/services/services.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [  
+  { path: 'all-users',  component: UsersComponent },
+  { path: 'all-orders',  component: OrdersComponent },
+  { path: 'all-services',  component: ServicesComponent },
+  { path: '', redirectTo: 'all-users', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './backend/users/users.component';
+import { HeaderComponent } from './backend/common/header/header.component';
+import { ServicesComponent } from './backend/services/services.component';
+import { OrdersComponent } from './backend/orders/orders.component';
+import { AddServicesComponent } from './backend/services/add-services/add-services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    HeaderComponent,
+    ServicesComponent,
+    OrdersComponent,
+    AddServicesComponent
   ],
   imports: [
     BrowserModule,
